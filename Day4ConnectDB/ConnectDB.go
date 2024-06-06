f72bffce1d580c74c38a8d6ce34abb4feca4145c
Duong Thanh Truong <truongnopro1111@gmail.com>
2024-06-06 12:43:50 +0700

    Day4: Connect DB

---
+++ Day4ConnectDB/ConnectDB.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"log"
+)
+
+func main() {
+	dsn := "root:@tcp(127.0.0.1:3306)/python_orm?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Kết nối thành công!")
+
+	rows, err := db.Query("SELECT id,name FROM student")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var id int
+		var name string
+		err = rows.Scan(&id, &name)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("ID: %d, Name: %s\n", id, name)
+	}
+
+	err = rows.Err()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
